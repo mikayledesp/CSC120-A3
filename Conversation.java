1ca81683c885b218798041835f4cbfd5cf456859
mikayledesp <mdespaigne@smith.edu>
2024-02-12 23:00:58 -0500

    Very primitive code just prints how interesting everytime

--- Conversation.java
+++ Conversation.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 class Conversation {
 
   public static void main(String[] arguments) {
     // You will start the conversation here.
+  Scanner sc = new Scanner(System.in);
+ System.out.println("Welcome to Chatbot! What do you have to say :)"); 
+  for (int i = 0; i < 5; i++) {
+    sc.nextLine();
+    System.out.println("How Interesting!");
+  }
+
+  
   }
 }
