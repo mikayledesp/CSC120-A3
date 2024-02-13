26d15e89a5596586ae4b7369a38bef5809b4833f
mikayledesp <mdespaigne@smith.edu>
2024-02-13 18:58:39 -0500

    Kind of works/premilimary framework

--- Conversation.java
+++ Conversation.java
@@ -1,16 +1,44 @@
 import java.util.Scanner;
+// import java.util.Random;
 
 class Conversation {
 
   public static void main(String[] arguments) {
     // You will start the conversation here.
-  Scanner sc = new Scanner(System.in);
- System.out.println("Welcome to Chatbot! What do you have to say :)"); 
-  for (int i = 0; i < 5; i++) {
-    sc.nextLine();
-    System.out.println("How Interesting!");
+ Scanner sc = new Scanner(System.in);
+ System.out.println("Welcome to Chatbot! How many rounds ?  :)"); 
+ int round = sc.nextInt();
+ sc.nextLine();
+ System.out.println("Hello! Whats on your mind?");
+ //Loop for conversation 
+  for (int i = 0 ; i < round ; i++) {
+    String input = sc.nextLine();
+    if (input.contains("I"))
+      System.out.println(input.replace("I", "You") + "?");
+
+    if (input.contains("me"))
+      System.out.println(input.replace("me", "You") + "?");
+    if (input.contains("am"))
+      System.out.println(input.replace("am", "are") + "?"); 
+    if (input.contains("you"))
+    System.out.println(input.replace("you", "I") + "?"); 
+    if (input.contains("my"))
+    System.out.println(input.replace("my", "your") + "?"); 
+    if (input.contains("your"))
+      System.out.println(input.replace("your", "my") + "?"); 
+      
+      
+
+   
   }
 
-  
+
+
+
+
+
+
+
+  sc.close();
   }
 }
