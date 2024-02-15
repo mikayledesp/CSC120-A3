ef086e336de3cb29be68fb11001a3177c895549f
mikayledesp <mdespaigne@smith.edu>
2024-02-15 16:16:12 -0500

    Okay i think this works!

--- Conversation.java
+++ Conversation.java
@@ -1,107 +1,97 @@
 import java.util.Scanner;
 import java.util.ArrayList;
-import java.util.List;
-// import java.util.Random;
+import java.util.Random;
 
 class Conversation {
-
   public static void main(String[] arguments) {
+  
+    Scanner userInput = new Scanner(System.in);
+    // list filled with filler responses 
+		ArrayList< String > filler = new ArrayList < String > ();
 
+        filler.add("Huh...");
+        filler.add("Wow, how cool...");
+        filler.add("Nice!");
+        filler.add("Really?");
+        filler.add("Awesome!");
+        filler.add("oh thats....");
+    ArrayList <String> transcript = new ArrayList<>();
+    //Note to self: in order to add to transcript list I might have to make each sout a string I can acess later i think idk
 
+    String greeting = "Hello, My name is Chatbot! How many rounds would you like? ";
+    System.out.println(greeting);
+    transcript.add(greeting); 
+    int round = userInput.nextInt();
+    userInput.nextLine(); //clear out
+    String rounds = String.valueOf(round);
+    transcript.add(rounds); 
 
-    // list of random phrases 
-    List<String> list=new ArrayList<String>();
-    list.add("Huh...");
-    list.add("Wow, how cool...");
-    list.add("Nice!");
-    list.add("Really?");
-    list.add("Awesome!");
-
+    String prompt = "Hey, what's on your mind?";
+    System.out.println(prompt);
+    transcript.add(prompt);
     
-    //list transcript 
+   for (int i=0; i < round; i++){
+    
+    String response = userInput.nextLine();
+    transcript.add(response);
+    
+ 
+    String[] split = response.split(" ");
+    String[] triggerWords = {"I", "me", "you", "my", "your", "am", "are"};
+    String[] altWords = {"you", "you", "me", "your", "my", "are", "am"};
+   
+    
+		int counter = 0;
+    String input = "";
+ 
+    for (int j=0; j < split.length; j++){
+        String word = "";
+        
+      for (int k=0; k<  triggerWords.length; k++){
+        if (split[j].equals(triggerWords[k])){
+            counter++;
+            word = altWords[k];
 
-    List<String> transcript=new ArrayList<String>();
-
-    // You will start the conversation here.
- Scanner sc = new Scanner(System.in);
- System.out.println("Welcome to Chatbot! How many rounds ?  :)"); 
- int round = sc.nextInt();
- sc.nextLine();
- System.out.println("Hello! Whats on your mind?");
- //Loop for conversation 
-  for (int i = 0 ; i < round ; i++) {
-    String input = sc.nextLine();
-    transcript.add(input);
-    if (input.contains("I"))
-      System.out.println(input.replace("I", "You") + "?");
+          }
+        }
+        if (word.equals("")){
+          input += split[j] + " ";
+        } else {
+          input += word + " "; 
+        }
      
 
-    if (input.contains("me"))
-      System.out.println(input.replace("me", "You") + "?");
-      
-
-    if (input.contains("am"))
-      System.out.println(input.replace("am", "are") + "?"); 
-      
-
-    if (input.contains("you"))
-      System.out.println(input.replace("you", "I") + "?"); 
-     
-
-    if (input.contains("my"))
-      System.out.println(input.replace("my", "your") + "?"); 
-      
-
-    if (input.contains("your"))
-      System.out.println(input.replace("your", "my") + "?"); 
-      
-
-    // else { 
-    //   Random rand = new Random();
-    //   int rand_int1 = rand.nextInt(5);
-    //   String element = list.get(rand_int1); 
-    //   System.out.println(element);
-
-
-    // }
+    }
+    if (counter > 0) {
+      System.out.println(input);
+      transcript.add(input); 
+    }
+    if (counter <= 0){
+       Random rand = new Random();
+       int rand_int1 = rand.nextInt(6);
+       String element = filler.get(rand_int1);
+       System.out.println(element);
+       transcript.add(element);
     
-
-
-
-
-
   }
-
-  // transcript of convo
-  System.out.println("Goodbye !");
-  System.out.println("Transcript :");
-  System.out.println(transcript);
+  
+  
+            
+    }
+    userInput.close();
+    
+    
+  
+  System.out.println("Goodbye!");
+  System.out.println("*****Transcript***** \n ");
+  System.out.println(transcript);}
+  
+   }
 
   
-  //Note
-  //might need to seperate responses from user and chat bot into different lists then print each index list
 
-
-
-sc.close();
-  }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  }
-
+    
+  
 
 
 
