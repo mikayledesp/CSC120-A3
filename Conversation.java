0205c750eb7f00c93c40d7f19413f8b7355e355c
mikayledesp <mdespaigne@smith.edu>
2024-02-15 16:52:07 -0500

    Getting closer, just need to fix transcript!

--- Conversation.java
+++ Conversation.java
@@ -6,49 +6,48 @@ class Conversation {
   public static void main(String[] arguments) {
   
     Scanner userInput = new Scanner(System.in);
-    // list filled with filler responses 
-		ArrayList< String > filler = new ArrayList < String > ();
-
-        filler.add("Huh...");
-        filler.add("Wow, how cool...");
-        filler.add("Nice!");
-        filler.add("Really?");
-        filler.add("Awesome!");
-        filler.add("oh thats....");
+		ArrayList < String > list = new ArrayList < String > ();
+        list.add("Huh...");
+        list.add("Wow, how cool...");
+        list.add("Nice!");
+        list.add("Really?");
+        list.add("Awesome!");
     ArrayList <String> transcript = new ArrayList<>();
-    //Note to self: in order to add to transcript list I might have to make each sout a string I can acess later i think idk
 
-    String greeting = "Hello, My name is Chatbot! How many rounds would you like? ";
-    System.out.println(greeting);
-    transcript.add(greeting); 
-    int round = userInput.nextInt();
+    String round = "Hello, My name is Chatbot! How many rounds would you like? ";
+    System.out.println(round);
+    transcript.add(round); 
+    int numofRound = userInput.nextInt();
     userInput.nextLine(); //clear out
-    String rounds = String.valueOf(round);
+    String rounds = String.valueOf(numofRound);
     transcript.add(rounds); 
 
     String prompt = "Hey, what's on your mind?";
+  
+  
     System.out.println(prompt);
     transcript.add(prompt);
     
-   for (int i=0; i < round; i++){
+   for (int i=0; i < numofRound; i++){
     
     String response = userInput.nextLine();
     transcript.add(response);
     
  
-    
-    String[] triggerWords = {"I ", " me ", " you ", " my ", " your ", " am ", " are "};
-    String[] altWords = {" you ", " you ", " me ", " your ", " my ", " are ", " am "};
     String[] split = response.split(" ");
-    
+    String[] triggerWords = {"I", "me", "you", "my", "your", "am", "are"};
+    String[] altWords = {"you", "you", "me", "your", "my", "are", "am"};
+  
+		//counter to help kick in the filler reponses 
 		int counter = 0;
+		//rebuilds the sentences i think??
     String input = "";
  
     for (int j=0; j < split.length; j++){
         String word = "";
         
       for (int k=0; k<  triggerWords.length; k++){
-        if (split[j].equals(triggerWords[k])){
+        if (split[j].equalsIgnoreCase(triggerWords[k])){
             counter++;
             word = altWords[k];
 
@@ -68,8 +67,8 @@ class Conversation {
     }
     if (counter <= 0){
        Random rand = new Random();
-       int rand_int1 = rand.nextInt(6);
-       String element = filler.get(rand_int1);
+       int rand_int1 = rand.nextInt(5);
+       String element = list.get(rand_int1);
        System.out.println(element);
        transcript.add(element);
     
@@ -82,22 +81,8 @@ class Conversation {
     
     
   
-  System.out.println("Goodbye! \n");
-  System.out.println("*****Transcript***** \n ");
-  System.out.println(transcript + "\n");}
+  System.out.println("Goodbye!");
+          System.out.println("*****Transcript*****");
+            System.out.println(transcript);}
   
    }
-
-  
-
-    
-  
-
-
-
-
-
-
-
-
-
